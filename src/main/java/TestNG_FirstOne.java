c38b3eb7be17090b24834ab8858158efd1c4c146
firas <firas.abu.sneneh@hotmail.com>
2022-08-27 12:54:44 +0300

    testNG

--- src/main/java/TestNG_FirstOne.java
+++ src/main/java/TestNG_FirstOne.java
@@ -1,2 +1,6 @@
 public class TestNG_FirstOne {
+
+    public int calculateSum (int number1, int number2){
+        return number1+number2;
+    }
 }
--- src/test/java/TestNG_test.java
+++ src/test/java/TestNG_test.java
@@ -1,2 +1,25 @@
+
+import org.junit.BeforeClass;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+import static org.testng.Assert.assertEquals;
+
+
 public class TestNG_test {
+
+    @BeforeClass
+    public void sayHi(){
+        System.out.println("hello");
+    }
+
+    @Parameters({"number1" , "number2", "sum"})
+    @Test
+    public void testNumSum(int number1, int number2, int sum){
+        TestNG_FirstOne cal=new TestNG_FirstOne();
+        int test1 = cal.calculateSum(number1,number2);
+        assertEquals(test1 , sum);
+    }
+
+
+
 }
